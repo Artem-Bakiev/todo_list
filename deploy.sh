1c560d7ddbdd29d7bb539a510abeb6d038bb9fde
Artem Bakiev <artembakiev@gmail.com>
2020-11-30 17:45:39 +0800

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:artem-bakiev/todo_list_hw.git master:gh-pages
+git push -f git@github.com:artem-bakiev/todo_list_hw.git main:gh-pages
 
 cd -
